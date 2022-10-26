825d01bcf01c7fdf22b3f04d42e5b583009640b2
golikovartyom <golikovartem404@gmail.com>
2022-10-26 13:11:34 +0400

    Add login screen

--- SpaceChat/Sources/ViewControllers/LoginViewController.swift
+++ SpaceChat/Sources/ViewControllers/LoginViewController.swift
@@ -1,5 +1,5 @@
 //
-//  SignUpViewController.swift
+//  LoginViewController.swift
 //  SpaceChat
 //
 //  Created by User on 26.10.2022.
@@ -7,27 +7,30 @@
 
 import UIKit
 
-class SignUpViewController: UIViewController {
+class LoginViewController: UIViewController {
 
-    let welcomeLabel = UILabel(text: "Hello", font: .avenir26())
+    let welcomeLabel = UILabel(text: "Welcome back!", font: .avenir26())
+    let loginWithLabel = UILabel(text: "Login with")
+    let orLabel = UILabel(text: "or")
     let emailLabel = UILabel(text: "Email")
-    let emailTextField = OneLineTextField()
     let passwordLabel = UILabel(text: "Password")
-    let passwordTextField = OneLineTextField()
-    let confirmPasswordLabel = UILabel(text: "Confirm password")
-    let confirmPasswordTextField = OneLineTextField()
-    let alreadyOnboardLabel = UILabel(text: "Already onboard?")
-    let signUpButton = UIButton(title: "Sign Up", titleColor: .white, backgroundColor: .buttonBlack(), cornerRadius: 8)
-    let loginButton: UIButton = {
+    let needAccountLabel = UILabel(text: "Need an account?")
+
+    let googleButton = UIButton(title: "Google", titleColor: .black, backgroundColor: .white, isShadow: true)
+    let loginButton = UIButton(title: "Login", titleColor: .white, backgroundColor: .buttonBlack())
+    let signUpButton: UIButton = {
         let button = UIButton(type: .system)
-        button.setTitle("Login", for: .normal)
+        button.setTitle("Sign Up", for: .normal)
         button.setTitleColor(UIColor.buttonRed(), for: .normal)
         button.titleLabel?.font = .avenir20()
         return button
     }()
+
+    let emailTextField = OneLineTextField()
+    let passwordTextField = OneLineTextField()
+
     let emailStack = UIStackView(axis: .vertical, spacing: 0)
     let passwordStack = UIStackView(axis: .vertical, spacing: 0)
-    let confirmPasswordStack = UIStackView(axis: .vertical, spacing: 0)
     let mainStackView = UIStackView(axis: .vertical, spacing: 40)
     let bottomStackView = UIStackView(axis: .horizontal, spacing: 15)
 
@@ -40,19 +43,19 @@ class SignUpViewController: UIViewController {
 
     private func setupHierarchy() {
         view.addSubview(welcomeLabel)
+        let loginWithView = ButtonView(label: loginWithLabel, button: googleButton)
         emailStack.addArrangedSubview(emailLabel)
         emailStack.addArrangedSubview(emailTextField)
         passwordStack.addArrangedSubview(passwordLabel)
         passwordStack.addArrangedSubview(passwordTextField)
-        confirmPasswordStack.addArrangedSubview(confirmPasswordLabel)
-        confirmPasswordStack.addArrangedSubview(confirmPasswordTextField)
+        mainStackView.addArrangedSubview(loginWithView)
+        mainStackView.addArrangedSubview(orLabel)
         mainStackView.addArrangedSubview(emailStack)
         mainStackView.addArrangedSubview(passwordStack)
-        mainStackView.addArrangedSubview(confirmPasswordStack)
-        mainStackView.addArrangedSubview(signUpButton)
-        bottomStackView.addArrangedSubview(alreadyOnboardLabel)
-        bottomStackView.addArrangedSubview(loginButton)
+        mainStackView.addArrangedSubview(loginButton)
         view.addSubview(mainStackView)
+        bottomStackView.addArrangedSubview(needAccountLabel)
+        bottomStackView.addArrangedSubview(signUpButton)
         view.addSubview(bottomStackView)
     }
 
@@ -62,12 +65,12 @@ class SignUpViewController: UIViewController {
             make.centerY.equalTo(view).multipliedBy(0.35)
         }
 
-        signUpButton.snp.makeConstraints { make in
+        loginButton.snp.makeConstraints { make in
             make.height.equalTo(60)
         }
 
         mainStackView.snp.makeConstraints { make in
-            make.centerY.equalTo(view.snp.centerY)
+            make.centerY.equalTo(view.snp.centerY).multipliedBy(1.1)
             make.leading.equalTo(view.snp.leading).offset(40)
             make.trailing.equalTo(view.snp.trailing).offset(-40)
         }
